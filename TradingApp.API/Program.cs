40b1b4f65d2c86255a5c1a8822eeae13b5c69fd3
miroslav-plancak <miroslav.plancak@tiacgroup.com>
2026-03-24 16:55:51 +0100

    added TradingDbContext; connectionString and entity models (#1)
    Co-authored-by: Miroslav <mplancak@gmail.com>

--- TradingApp.API/Program.cs
+++ TradingApp.API/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TradingApp.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<TradingDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ TradingApp.Domain/Models/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TradingApp.Domain.Models.Enums;
+
+namespace TradingApp.Domain.Models.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid ClientOrderId { get; set; }
+        public OrderStatus Status { get; set; }
+        public int Quantity { get; set; }   
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
