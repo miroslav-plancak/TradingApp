4c4324f56456676cef641d34d6454e4afd5fb0a3
Miroslav <mplancak@gmail.com>
2026-03-24 16:53:57 +0100

    added TradingDbContext; connectionString and entity models

---
+++ TradingApp.Domain/TradingDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradingApp.Domain.Models.Entities;
+
+namespace TradingApp.Domain
+{
+    public class TradingDbContext : DbContext
+    {
+        public TradingDbContext(DbContextOptions<TradingDbContext> options) : base(options) { }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.ClientOrderId).IsRequired();
+                entity.HasIndex(e => e.ClientOrderId).IsUnique();
+                entity.Property(e => e.Status).IsRequired();
+                entity.Property(e => e.Quantity).IsRequired();
+                entity.Property(e => e.Price).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.UpdatedAt).IsRequired();
+            });
+        }
+    }
+}
